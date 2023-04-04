6e57c0f9709a2faad4565e792dbdc9a4585ad72f
Andy Chen <chen.andy18@gmail.com>
2023-04-04 15:59:59 -0700

    minor value changes.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute xs:botom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:botom-10 bottom-0 w-full flex justify-center items-center">
         <a href='#about'>
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
