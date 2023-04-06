741c8663f8e2adea1de95d2be07aeb4d3c1f51c0
Andy Chen <chen.andy18@gmail.com>
2023-04-06 16:41:39 -0700

    earth complete.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -55,12 +55,15 @@ const Contact = () => {
               value={form.message}
             onChange={handleChange}
             placeholder="What do you want to say?"
-            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium" />
+              className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rounded-lg outline-none border-none font-medium resize-none" />
           </label>
           <button
             className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
             type="submit">{loading ? 'Sending...' : 'Send'}</button>
         </form>
+      </motion.div>
+      <motion.div variants={slideIn('right', 'tween', 0.2, 1)} className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'>
+        <EarthCanvas />
       </motion.div>
     </div>
   )
