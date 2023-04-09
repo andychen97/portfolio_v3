be03fb635fe7bbada5385ef3e32a69e0bda30991
Andy Chen <chen.andy18@gmail.com>
2023-04-08 17:21:22 -0700

    navbar complete.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,13 +5,27 @@ import { styles } from '../styles'
 import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
+const ResumeButton = () => {
+  fetch('/resume.pdf')
+    .then(response => {
+      response.blob()
+        .then(blob => {
+          const fileURL = window.URL.createObjectURL(blob);
+          const alink = document.createElement('a');
+          alink.href = fileURL;
+          alink.download = 'Andy_Chen_Resume.pdf';
+          alink.click();
+        });
+    });
+}
+
 const Navbar = () => {
   const [active, setActive] = useState('');
   const [toggle, setToggle] = useState(false);
 
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-2 fixed top-0 z-20 bg-primary`}>
-      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+    <nav className={`${styles.paddingX} w-full flex items-center p-2 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-10xl mx-auto">
         <Link
           to='/'
           className="flex items-center gap-2"
@@ -21,39 +35,47 @@ const Navbar = () => {
           }}>
             <img src={logo} alt='logo' className='w-12 h-12 object-contain' />
         </Link>
-        <ul className="list-none hidden sm:flex flex-row gap-10">
+        <ul className="list-none sm:hidden flex flex-row gap-6 items-center">
           {navLinks.map((link) => (
             <li
               key={link.id}
-              className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+              className={`${active === link.title ? "text-highlight" : "text-offWhite"} hover:text-highlight text-[12px] font-sfmono cursor-pointer pt-0.5`}
               onClick={() => setActive(link.title)}>
-              <a href={`#${link.id}`}>{link.title}</a>
+              <a href={`#${link.id}`}><span className='text-highlight'>{link.number}. </span>{link.title}</a>
             </li>
           ))}
+          <li>
+            <button className='text-highlight hover:bg-highlight hover:bg-opacity-25 border border-highlight rounded py-1.5 px-4 text-[12px] font-sfmono'  type='submit' onClick={ResumeButton}>Resume</button>
+          </li>
         </ul>
-        <div className='sm:hidden flex flex-1 justify-end items-center'>
-            <img
-              src={toggle ? close : menu}
-              alt='menu'
-              className='w-[28px] h-[28px] object-contain cursor-pointer'
-              onClick={() => setToggle(!toggle)}
-            />
 
-            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-              <ul className="list-none flex justify-end items-start flex-col gap-4">
-                {navLinks.map((link) => (
-                  <li
-                    key={link.id}
-                    className={`${active === link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`}
-                    onClick={() => {
-                      setToggle(!toggle);
-                      setActive(link.title);
-                      }}>
-                    <a href={`#${link.id}`}>{link.title}</a>
-                  </li>
-                ))}
-              </ul>
-            </div>
+
+
+        <div className='hidden sm:flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            onClick={() => setToggle(!toggle)}
+          />
+          <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+            <ul className="list-none flex items-center flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title ? "text-highlight" : "text-offWhite"} font-sfmono cursor-pointer text-[14px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                    }}>
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+              <li>
+                <button className='text-highlight hover:bg-highlight hover:bg-opacity-25 border border-highlight rounded py-2 px-4 text-[12px] font-sfmono' type='submit' onClick={ResumeButton}>Resume</button>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,31 @@ module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   mode: "jit",
   theme: {
+    fontFamily: {
+      'sfmono': ['SFMono', 'sans-serif'],
+      'calibre': ['Calibre', 'sans-serif']
+    },
     extend: {
       colors: {
-        primary: "#050816",
+        primary: "#181818",
         secondary: "#aaa6c3",
         tertiary: "#151030",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        offWhite: "#ccd6f6",
+        offGrey: "#8892b0",
+        highlight: "#62f8d5"
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
       },
       screens: {
-        xs: "450px",
+        'xs': { 'min': '375px', 'max': '499px' },
+        'sm': { 'min': '500px', 'max': '767px' },
+        'md': { 'min': '768px', 'max': '900px' },
+        'lg': { 'min': '901px', 'max': '1025px' },
+        'xl': { 'min': '1170px'}
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
