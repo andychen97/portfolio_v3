ed945acea2e72e5d45c92543fb9fcd82e3d73bd8
Andy Chen <chen.andy18@gmail.com>
2023-04-09 19:04:35 -0700

    about-update complete.

--- src/assets/index.js
+++ src/assets/index.js
@@ -13,10 +13,9 @@ import figma from "./tech/figma.png";
 import git from "./tech/git.png";
 import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
 import nodejs from "./tech/nodejs.png";
+import postgresql from "./tech/postgresql.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
@@ -45,10 +44,9 @@ export {
   git,
   html,
   javascript,
-  mongodb,
   nodejs,
+  postgresql,
   reactjs,
-  redux,
   tailwind,
   typescript,
   threejs,
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,13 @@ import { styles } from '../styles'
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
 import { SectionWrapper } from '../hoc';
+import { Tech } from '../components';
+import Selfie from '/selfie.jpg'
 
 //change titles in constants/index.js
 const ServiceCard = ({ index, title, icon}) => {
   return (
-    <Tilt className='xs:w-[250px] w-full'>
+    <Tilt className='xs:w-full w-[250px]'>
       <motion.div
         variants={fadeIn('right', 'spring', 0.5 * index, 0.75)}
         className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
@@ -38,16 +40,42 @@ const About = () => {
         <p className={styles.sectionSubText}>Introduction</p>
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
-      <motion.p
-        variant={fadeIn('', '', 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-        Hey! I am a software developer with a background in Information Systems and Web Development. I graduated from San Jose State University in the Spring of 2020 during the onset of the pandemic. Despite the challenging job market, I utilized this time to expand my knowledge by self-educating in finance and options trading. It was quite a profitable experience. However, as the world began to reopen, I wanted to refresh my skills in Python and mySQL and discovered a passion for web development through an online JavaScript course. <br /><br />
-        I subsequently enrolled in an accelerated, in-person full-stack web development program in Irvine, California where I acquired comprehensive skills in HTML, CSS, JavaScript, Node.js, Express.js, PostgreSQL, React, data structures and algorithms. Having completed the program, I am currently working as a Web Dev TA and continue to improve my skills through daily leetcoding. I am now seeking new opportunities to apply my skills in the web development industry.
-      </motion.p>
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="flex flex-wrap">
+        <motion.p
+          variant={fadeIn('', '', 0.1, 1)}
+          className='xs:basis-full sm:basis-full xs:order-2 sm:order-2 order-1 basis-3/5 mt-4 text-secondary font-light text-[18px] leading-7'>
+          Hey! I am a software developer with a background in Information Systems
+          and Web Development. I graduated from San Jose State University in the
+          Spring of 2020 during the onset of the pandemic. Despite the challenging
+          job market, I utilized this time to expand my knowledge by self-educating
+          in finance and options trading. It was quite a profitable experience.
+          However, as the world began to reopen, I wanted to refresh my skills in
+          Python and mySQL and discovered a passion for web development through an
+          online JavaScript course.
+          <br />
+          <br />
+          I subsequently enrolled in an accelerated, in-person full-stack web
+          development program in Irvine, California where I acquired comprehensive
+          skills in HTML, CSS, JavaScript, Node.js, Express.js, PostgreSQL, React,
+          data structures and algorithms. Having completed the program, I am
+          currently working as a Web Dev TA and continue to improve my skills
+          through daily leetcoding. I am now seeking new opportunities to apply
+          my skills in the web development industry.
+          <br />
+          <br />
+          Here are some technologies that I have been working with recently:
+        </motion.p>
+        <div className="xs:basis-full sm:basis-full basis-2/5 xs:order-1 sm:order-1 order-2">
+          <div className="flex justify-center">
+            <img src={Selfie} alt='selfie' className={styles.selfie} />
+          </div>
+        </div>
+      </div>
+      <Tech />
+      <div className="justify-center mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
-        ))}
+          ))}
       </div>
     </>
   )
