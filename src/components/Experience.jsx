74dedb9eca5185e9464c63dd9a929465afeaced3
Andy Chen <chen.andy18@gmail.com>
2023-04-09 20:08:57 -0700

    experience page updated.

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,27 +9,37 @@ import { textVariant } from "../utils/motion";
 
 
 const ExperienceCard = ({ experience }) => {
+  let paddingTop;
+  let paddingBottom;
+  if (experience.points.length === 0) {
+    paddingTop = '20px';
+    paddingBottom = '0px';
+  } else {
+    paddingTop = '24px';
+    paddingBottom = '24px';
+  }
   return (
     <VerticalTimelineElement
-      contentStyle={{ background: '#1d1836', color:'#fff' }}
-      contentArrowStyle={{ borderRight: '7px solid #232631'}}
+      contentStyle={{ background: 'rgb(30,37,48)', color: '#D0D1D2', 'padding-top': paddingTop, 'padding-bottom': paddingBottom}}
+      contentArrowStyle={{ borderRight: '9px solid  rgb(30,37,48)' }}
       date={experience.date}
-      iconStyle={{ background: experience.iconBg }}
+      iconStyle={{ background: experience.iconBg}}
       icon={
         <div className="flex justify-center items-center w-full h-full">
           <img
             src={experience.icon}
             alt={experience.company_name}
-            className='w-[60%] h-[60%] object-contain' />
+            className='w-[80%] h-[80%] object-contain' />
         </div>}>
         <div>
           <h3 className="text-white text-[24px] text-bold">{experience.title}</h3>
           <p className="text-secondary, text-[16px] font-semibold" style={{ margin: 0 }}>{experience.company_name}</p>
+          <p className="text-secondary, text-[16px] font-semibold" style={{ margin: 0 }}>{experience.location}</p>
           <ul className="mt-5 list-disc ml-5 space-y-2">
             {experience.points.map((point, index) => (
               <li
-                key={`experience-poinbt-${index}`}
-                className='text-white-100 text-[14px] pl-1 tracking-wider'>
+                key={`experience-point-${index}`}
+                className='font-calibre text-white-100 text-[14px] pl-1 tracking-wider'>
                   {point}
               </li>
             ))}
@@ -47,8 +57,9 @@ const Experience = () => {
         <p className={styles.sectionSubText}>What I have done so far</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
       </motion.div>
-      <div className="mt-20 flex flex-col">
-        <VerticalTimeline>
+      <div className="mt-6 flex flex-col">
+        <VerticalTimeline
+          lineColor='#ccd6f6'>
           {experiences.map((experience, index) => (
             <ExperienceCard key={index} experience={experience} />
           ))}
